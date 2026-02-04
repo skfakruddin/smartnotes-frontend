9c2c528a02a9c0dc264398b36f6046accfdb5811
SHAIK FAKRUDDIN <shaikfakruddin2145@gmail.com>
2026-02-04 19:47:42 +0530

    Update tailwind.config.js

--- smartnotes-frontend/tailwind.config.js
+++ smartnotes-frontend/tailwind.config.js
@@ -1,11 +1,8 @@
-// tailwind.config.js
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
-    // other paths where Tailwind should look for classes
   ],
   plugins: [
     require('tailwind-scrollbar-hide'),
-    // other plugins if you have them
   ],
 };
